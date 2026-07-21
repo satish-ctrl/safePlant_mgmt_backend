cf084a194cfab7e46130d7dc2115855d7a81b3b2
Saurabh Srivastav <155880426+Saurabh12325@users.noreply.github.com>
2026-07-22 03:40:27 +0530

    Feature/saurabh/updation (#15)
    * addd simulatot change
    
    * added sensor simulator
    
    * delete unnecessary file
    
    * delete unnecessary file data
    
    * deploy testing
    
    * deploy tesing
    
    * new deploy testing
    
    * new changes
    
    * deploy tesing new
    
    * final testing
    
    * chore: untrack build target directory and fix deployment merge conflicts
    
    * final testing
    
    * camera integration
    
    * change mode
    
    * small changes
    
    * remove codes
    
    * new changes in code
    
    * added roles
    
    ---------
    
    Co-authored-by: satish-ctrl <satishchand2425@gmail.com>

--- backend/src/main/java/com/safeops/backend/dto/request/RegisterRequest.java
+++ backend/src/main/java/com/safeops/backend/dto/request/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.safeops.backend.dto.request;
 
+import com.safeops.backend.entity.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -18,4 +19,6 @@ public class RegisterRequest {
     @NotBlank(message = "Password is required")
     @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
     private String password;
+
+    private Role role;
 }
